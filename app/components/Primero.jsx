6b3cd2fd8c59fa3028ae2be2494ca15e852351ca
Leandro Martini <87733423+manrique1986@users.noreply.github.com>
2025-03-28 20:30:28 -0300

    modificando tamaño

--- app/components/Primero.jsx
+++ app/components/Primero.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { FaCircle } from "react-icons/fa";
 
 const Primero = () => {
   return (
     <div
-      className="flex flex-col items-center sm:items-start h-auto min-h-screen w-full text-white p-6 sm:p-8 bg-cover bg-center bg-no-repeat
-     bg-[url('https://res.cloudinary.com/dytpump6i/image/upload/v1743032584/Disen%CC%83o_sin_ti%CC%81tulo_20_vc51tk.png')]"
-      style={{ backgroundSize: 'cover', backgroundPosition: 'center' }}
+      className="relative flex flex-col items-center sm:items-start h-auto min-h-screen w-full text-white  sm:p-8 bg-cover bg-center bg-no-repeat"
+      style={{ backgroundImage: "url('https://res.cloudinary.com/dytpump6i/image/upload/v1743202079/Disen%CC%83o_sin_ti%CC%81tulo_22_qp9en7.png')" }}
     >
-      <div className="flex flex-col items-center sm:items-start justify-center w-full max-w-[90%] sm:w-3/5 p-6 sm:p-8 rounded-lg sm:pl-16">
+      {/* Fondo para móviles */}
+      <div className="absolute inset-0 bg-cover bg-center bg-no-repeat sm:hidden" 
+        style={{ backgroundImage: "url('https://res.cloudinary.com/dytpump6i/image/upload/v1743202446/Disen%CC%83o_sin_ti%CC%81tulo_24_s9ecqz.png')" }}>
+      </div>
+
+      <div className="relative flex flex-col items-center sm:items-start justify-center w-full max-w-[100%] sm:w-3/5 p-6 sm:p-8 rounded-lg sm:pl-16">
         <div className="flex items-center justify-center sm:justify-start gap-4 sm:gap-6">
           <h1 className="text-3xl sm:text-8xl font-[var(--font-antonio)]">AT</h1>
           <div className="border-l-4 border-white h-12 sm:h-24"></div>
@@ -29,44 +34,41 @@ const Primero = () => {
 
           <div className="mt-6 text-center sm:text-left p-4">
             <div className="flex flex-wrap justify-center sm:justify-start gap-8">
-              {/* Contenedor principal para las dos listas */}
-              <div className="w-full sm:w-auto">
-                <ul className="space-y-2 font-[var(--font-poppins)] text-xs sm:text-base sm:grid sm:grid-cols-2 sm:gap-8 sm:space-y-0">
+              <div className="w-full sm:w-auto ">
+                <ul className="space-y-2 font-[var(--font-poppins)] text-xs sm:text-base grid grid-cols-2 sm:gap-8 sm:space-y-0">
                   <li className="flex items-center gap-3">
-                    <span className="w-2 h-2 sm:w-3 sm:h-3 bg-[#911ba2] rounded-full"></span> ESTRATEGIA de captación personalizada
+                    <FaCircle className="text-[#911ba2] text-xs " /> ESTRATEGIA de captación personalizada
                   </li>
                   <li className="flex items-center gap-3">
-                    <span className="w-2 h-2 sm:w-3 sm:h-3 bg-[#911ba2] rounded-full"></span> PUBLICIDAD OPTIMIZADA en Meta y Google
+                    <FaCircle className="text-[#911ba2] text-xs " /> PUBLICIDAD OPTIMIZADA en Meta y Google
                   </li>
                   <li className="flex items-center gap-3">
-                    <span className="w-2 h-2 sm:w-3 sm:h-3 bg-[#911ba2] rounded-full"></span> OPTIMIZACIÓN DE PERFIL en redes
+                    <FaCircle className="text-[#911ba2] text-xs " /> OPTIMIZACIÓN DE PERFIL en redes
                   </li>
                   <li className="flex items-center gap-3">
-                    <span className="w-2 h-2 sm:w-3 sm:h-3 bg-[#911ba2] rounded-full"></span> CREATIVOS de alto impacto
+                    <FaCircle className="text-[#911ba2] text-xs " /> CREATIVOS de alto impacto
                   </li>
                 </ul>
               </div>
 
-              {/* Contenedor principal para la segunda lista */}
               <div className="w-full sm:w-auto">
-                <ul className="space-y-2 font-[var(--font-poppins)] text-xs sm:text-base sm:grid sm:grid-cols-2 sm:gap-8 sm:space-y-0">
+                <ul className="space-y-2 font-[var(--font-poppins)] text-xs sm:text-base grid grid-cols-2 sm:gap-8 sm:space-y-0">
                   <li className="flex items-center gap-3">
-                    <span className="w-2 h-2 sm:w-3 sm:h-3 bg-[#911ba2] rounded-full"></span> AUTOMATIZACIÓN de gestión de clientes
+                    <FaCircle className="text-[#911ba2] text-xs " /> AUTOMATIZACIÓN de gestión de clientes
                   </li>
                   <li className="flex items-center gap-3">
-                    <span className="w-2 h-2 sm:w-3 sm:h-3 bg-[#911ba2] rounded-full"></span> REPORTES y ANÁLISIS de resultados
+                    <FaCircle className="text-[#911ba2] text-xs " /> REPORTES y ANÁLISIS de resultados
                   </li>
                   <li className="flex items-center gap-3">
-                    <span className="w-2 h-2 sm:w-3 sm:h-3 bg-[#911ba2] rounded-full"></span> PLANTILLAS de respuesta para ventas
+                    <FaCircle className="text-[#911ba2] text-xs " /> PLANTILLAS de respuesta para ventas
                   </li>
                   <li className="flex items-center gap-3">
-                    <span className="w-2 h-2 sm:w-3 sm:h-3 bg-[#911ba2] rounded-full"></span> SEGUIMIENTO y AJUSTES semanales
+                    <FaCircle className="text-[#911ba2] text-xs " /> SEGUIMIENTO y AJUSTES semanales
                   </li>
                 </ul>
               </div>
             </div>
 
-            {/* Botón central */}
             <div className="flex justify-center sm:justify-start mt-6">
               <button className="px-4 sm:px-6 py-2 sm:py-3 bg-[#911ba3] text-white text-xs sm:text-lg rounded-lg hover:bg-[#000000] font-[var(--font-antonio)] text-center w-full sm:w-auto">
                 NO NECESITÁS MÁS SEGUIDORES, NECESITÁS MÁS CLIENTES. Te muestro cómo en una ASESORÍA GRATUITA.
@@ -80,6 +82,8 @@ const Primero = () => {
 };
 
 export default Primero;
+
+
 
 
 

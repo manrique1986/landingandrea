667c741de4232ec14182bb3003260a6b13f46e34
Leandro Martini <87733423+manrique1986@users.noreply.github.com>
2025-03-28 20:53:34 -0300

    arreglando home

--- app/components/Primero.jsx
+++ app/components/Primero.jsx
@@ -12,7 +12,7 @@ const Primero = () => {
         style={{ backgroundImage: "url('https://res.cloudinary.com/dytpump6i/image/upload/v1743202446/Disen%CC%83o_sin_ti%CC%81tulo_24_s9ecqz.png')" }}>
       </div>
 
-      <div className="relative flex flex-col items-center sm:items-start justify-center w-full max-w-[100%] sm:w-3/5 p-6 sm:p-8 rounded-lg sm:pl-16">
+      <div className="relative flex flex-col items-center sm:items-start justify-center w-full max-w-[100%] sm:w-3/5  sm:p-8 rounded-lg sm:pl-16">
         <div className="flex items-center justify-center sm:justify-start gap-4 sm:gap-6">
           <h1 className="text-3xl sm:text-8xl font-[var(--font-antonio)]">AT</h1>
           <div className="border-l-4 border-white h-12 sm:h-24"></div>
@@ -32,44 +32,44 @@ const Primero = () => {
             Dejá de gastar tiempo y dinero en estrategias que no generan RESULTADOS. Con nuestro MÉTODO probado, vas a atraer CLIENTES LISTOS PARA COMPRAR sin necesidad de estar 24/7 en redes.
           </p>
 
-          <div className="mt-6 text-center sm:text-left p-4">
-            <div className="flex flex-wrap justify-center sm:justify-start gap-8">
+          <div className="mt-6 text-center sm:text-left p-4 ">
+            <div className="flex flex-wrap justify-center sm:justify-start lg:gap-8">
               <div className="w-full sm:w-auto ">
-                <ul className="space-y-2 font-[var(--font-poppins)] text-xs sm:text-base grid grid-cols-2 sm:gap-8 sm:space-y-0">
-                  <li className="flex items-center gap-3">
+                <ul className="space-y-7 font-[var(--font-poppins)] text-xs sm:text-base grid grid-cols-2 sm:gap-8 sm:space-y-0">
+                  <li className="flex   gap-2">
                     <FaCircle className="text-[#911ba2] text-xs " /> ESTRATEGIA de captación personalizada
                   </li>
-                  <li className="flex items-center gap-3">
+                  <li className="flex  gap-2">
                     <FaCircle className="text-[#911ba2] text-xs " /> PUBLICIDAD OPTIMIZADA en Meta y Google
                   </li>
-                  <li className="flex items-center gap-3">
+                  <li className="flex  gap-2">
                     <FaCircle className="text-[#911ba2] text-xs " /> OPTIMIZACIÓN DE PERFIL en redes
                   </li>
-                  <li className="flex items-center gap-3">
+                  <li className="flex  gap-2">
                     <FaCircle className="text-[#911ba2] text-xs " /> CREATIVOS de alto impacto
                   </li>
                 </ul>
               </div>
 
               <div className="w-full sm:w-auto">
-                <ul className="space-y-2 font-[var(--font-poppins)] text-xs sm:text-base grid grid-cols-2 sm:gap-8 sm:space-y-0">
-                  <li className="flex items-center gap-3">
+                <ul className="space-y-4  font-[var(--font-poppins)] text-xs sm:text-base grid grid-cols-2 sm:gap-8 sm:space-y-0">
+                  <li className="flex  gap-2">
                     <FaCircle className="text-[#911ba2] text-xs " /> AUTOMATIZACIÓN de gestión de clientes
                   </li>
-                  <li className="flex items-center gap-3">
+                  <li className="flex  gap-2">
                     <FaCircle className="text-[#911ba2] text-xs " /> REPORTES y ANÁLISIS de resultados
                   </li>
-                  <li className="flex items-center gap-3">
+                  <li className="flex  gap-2">
                     <FaCircle className="text-[#911ba2] text-xs " /> PLANTILLAS de respuesta para ventas
                   </li>
-                  <li className="flex items-center gap-3">
+                  <li className="flex  gap-2">
                     <FaCircle className="text-[#911ba2] text-xs " /> SEGUIMIENTO y AJUSTES semanales
                   </li>
                 </ul>
               </div>
             </div>
 
-            <div className="flex justify-center sm:justify-start mt-6">
+            <div className="flex justify-center sm:justify-start mt-20">
               <button className="px-4 sm:px-6 py-2 sm:py-3 bg-[#911ba3] text-white text-xs sm:text-lg rounded-lg hover:bg-[#000000] font-[var(--font-antonio)] text-center w-full sm:w-auto">
                 NO NECESITÁS MÁS SEGUIDORES, NECESITÁS MÁS CLIENTES. Te muestro cómo en una ASESORÍA GRATUITA.
               </button>

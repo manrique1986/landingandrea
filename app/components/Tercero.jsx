2ef6e4850055e0f111f6281c64b51250c3693ec6
Leandro Martini <87733423+manrique1986@users.noreply.github.com>
2025-03-29 12:30:17 -0300

    modo oscuro??

--- app/components/Tercero.jsx
+++ app/components/Tercero.jsx
@@ -6,7 +6,7 @@ import Carrousel from "./ui/Carrousel";
 const TestimonialsSection = () => {
   return (
     <section className="bg-[#600f6d] py-16">
-      <div className="container mx-auto px-4 text-center">
+      <div className="relative z-10 container mx-auto px-4 text-center">
         <h2 className="text-4xl font-bold mb-8">
           ALGUNOS TESTIMONIOS
         </h2>

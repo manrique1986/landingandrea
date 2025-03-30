7db98a6ef0a2ae96eed38bbecc604ea0a1355fd2
Leandro Martini <87733423+manrique1986@users.noreply.github.com>
2025-03-30 14:34:01 -0300

    dark1

--- app/components/quinto.jsx
+++ app/components/quinto.jsx
@@ -31,7 +31,7 @@ export default function FAQ() {
   return (
     <div className="bg-gradient-to-b from-[#600f6d] to-[#440155] opacity-80 backdrop-blur-lg">
     <div className="max-w-2xl mx-auto p-4">
-      <h2 className="text-2xl font-bold text-center mb-6">Preguntas Frecuentes</h2>
+      <h2 className="text-2xl font-bold text-center mb-6 text-white dark:text-white">Preguntas Frecuentes</h2>
       <div className="space-y-4">
         {faqs.map((faq, index) => (
           <div key={index} className="border rounded-lg p-4 bg-[#600f6d]">

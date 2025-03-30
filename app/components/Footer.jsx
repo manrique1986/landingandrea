854dab730abbf253700090f965b6f8e0cd47cf04
Leandro Martini <87733423+manrique1986@users.noreply.github.com>
2025-03-30 17:22:39 -0300

    footer1

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -5,21 +5,19 @@ import { FaPhone, FaEnvelope } from 'react-icons/fa';
 const Footer = () => {
   return (
     <footer className="bg-[#222222] text-white py-4 w-full">
-      <div className="max-w-7xl mx-auto flex justify-between items-center px-6 sm:px-4">
-        <div className="text-lg font-semibold sm:text-base text-center sm:text-left">
+      <div className="max-w-7xl mx-auto flex flex-col sm:flex-row justify-between items-center px-6 sm:px-4">
+        <div className="text-lg font-semibold sm:text-base text-center sm:text-left mb-4 sm:mb-0">
           Andrea Tomatis
         </div>
 
-        <div className="flex flex-wrap justify-center sm:justify-between space-x-6 sm:space-x-4 mt-2 sm:mt-0">
+        <div className="flex flex-wrap justify-center sm:justify-between w-full sm:w-auto space-x-6 sm:space-x-4 mt-4 sm:mt-0">
           <a
-            
-            className="text-lg font-medium hover:underline sm:text-sm w-full sm:w-auto text-center flex items-center justify-center space-x-2"
+            className="text-lg font-medium hover:underline sm:text-sm w-full sm:w-auto text-center flex items-center justify-center space-x-2 mb-2 sm:mb-0"
           >
             <FaPhone />
             <span>+54 9 2612 47-2068</span>
           </a>
           <a
-           
             className="text-lg font-medium hover:underline sm:text-sm w-full sm:w-auto text-center flex items-center justify-center space-x-2"
           >
             <FaEnvelope />
